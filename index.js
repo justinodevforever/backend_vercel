a6557bd2fd20d5d3df41ef6576c531429442bca4
justinodevforever <jchitombi7@gmail.com>
2024-02-17 00:19:14 +0100

    atualizar o

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from "express";
 // import router from "./src/router/routas";
-// import routerh from "./src/router/home";
+import routerh from "./src/router/home.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/dd", routerh);
+app.use("/dd", routerh);
 app.get("/", (req, res) => {
   res.json("kkkkkkkkkkkkkkk");
 });
