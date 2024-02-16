d27d20c348463c320a13fbbb267a8221cf854b72
justinodevforever <jchitombi7@gmail.com>
2024-02-17 00:09:26 +0100

    atualizar outro mais

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router);
-app.use(routerh);
+app.use("/", routerh);
+// app.use(routerh);
 
 app.listen(3000, () => console.log("Servidor Rodando"));
